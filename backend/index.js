cce56819169bb64ee3928d288997080d9720812d
xnacro <businessofficialtech@gmail.com>
2026-07-12 10:32:47 +0530

    feat: setup PostgreSQL database connection

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,21 @@
 import express from "express";
 import dotenv from "dotenv";
+import pool from "./src/config/db.js";
 
 dotenv.config();
 
 const app = express();
 
 const port = process.env.PORT;
+
+try {
+    const result = await pool.query("SELECT NOW()");
+    console.log("PostgreSQL Connected");
+    console.log(result.rows[0]);
+} catch (err) {
+    console.error("Database Connection Failed");
+    console.error(err.message);
+}
 
 app.get("/", (req, res) => {
     res.send("Hello World! from TransitOps Backend");
