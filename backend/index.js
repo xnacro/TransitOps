88c624df6092f9c50eed9da8c51ae9cc9104a0df
xnacro <businessofficialtech@gmail.com>
2026-07-12 09:50:07 +0530

    chore: initialize backend project

---
+++ backend/index.js
@@ -0,0 +1,16 @@
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+const port = process.env.PORT;
+
+app.get("/", (req, res) => {
+    res.send("Hello World! from TransitOps Backend");
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
